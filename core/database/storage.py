6713aeb18ddf5c4018e5df4286a187c81eef3399
KENC0DE <kenansameseret@gmail.com>
2024-02-20 17:06:17 +0530

    db++

--- core/database/storage.py
+++ core/database/storage.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from core.models.base import BaseDB
 from os import getenv
+from models.player import Player
 
 
 class Storage:
@@ -30,6 +31,24 @@ class Storage:
         self.__session = make_session()
 
 
-    def list_all(self):
+    def list_all(self, inst=None):
         """ List everything in The storage. """
-        pass
+        if inst:
+            all_inst = self.__session.query(inst).all()
+        else:
+            all_inst = self.__session.query(Player).all()
+        
+        return all_inst
+
+
+    def save(self):
+        """ Save instance to the storage """
+        self.__session.commit()
+
+
+    def add(self, inst):
+        """ Add new instance to the storage """
+        if not inst:
+            return
+
+        self.__session.add(inst)
